357cde46c42edff7210193bb0bbccc3bdaadef9f
Riddle <turacode989@gmail.com>
2023-04-17 02:10:53 +0600

    Routing added

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "New York City"},
+                    new {id = 2, Name = "Antwerp"}
+                });
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -18,15 +18,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//app.UseRouting();
+
 app.UseAuthorization();
+
+//app.UseEndpoints(endpoints => {
+//    endpoints.MapControllers();
+//});
 
 app.MapControllers();
 
 // Below code output the stirng if there is no middleware configured
 // remove "swagger" from the url
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Hello world!");
-});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello world!");
+//});
 
 app.Run();
