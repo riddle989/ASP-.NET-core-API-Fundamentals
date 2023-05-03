8e9c9f4ddbf89c585242da698d6508871a43d7fc
Riddle <turacode989@gmail.com>
2023-05-03 17:56:46 +0600

    Implement Repository pattern in the Cities Controller

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -7,29 +8,48 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(CitiesDataStore citiesDataStore) 
+        public CitiesController(ICityInfoRepository cityInfoRepository) 
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
+
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities);
+            // We only make async method of data fetching from the database
+            // As soon as data retrived, we start processing the data, so don't need the asynchronous method
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+
+            var results = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var city in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = city.Id,
+                    Description = city.Description,
+                    Name = city.Name,
+                });
+            }
+
+            return Ok(results);
+            //return Ok(_citiesDataStore.Cities);
         }
 
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id) 
         {
-            var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+            //var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
 
-            if (cityToReturn == null) 
-            {
-                return NotFound();
-            }
+            //if (cityToReturn == null) 
+            //{
+            //    return NotFound();
+            //}
 
-            return Ok(cityToReturn);
+            //return Ok(cityToReturn);
+
+            return Ok();
         }
     }
 }
